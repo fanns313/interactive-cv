4563d5d2aa9e04aec13794ae435fda3ad42beb02
fanns313 <emailanda@example.com>
2025-07-11 00:17:24 -0700

    feat: setup fullstack for vercel deployment

---
+++ backend/data.js
@@ -0,0 +1,6 @@
+// backend/data.js
+const educationHistory = [ /* ...data... */ ];
+const skills = [ /* ...data... */ ];
+const projects = [ /* ...data... */ ];
+module.exports = { educationHistory, skills, projects };
+
